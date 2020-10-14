aacabfea9bf17e9bf76d2f0947c4c7e52eae816a
felipe <giribroba@gmail.com>
2020-10-13 22:39:27 -0300

    Demo2

---
+++ Assets/Script/Cam.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cam : MonoBehaviour
+{
+    public GameObject pCamera, portal;
+    private Camera camera;
+
+    void Awake()
+    {
+        camera = this.GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        this.transform.position = new Vector3(this.transform.position.x, pCamera.transform.position.y, pCamera.transform.position.z);
+        this.transform.forward = -pCamera.transform.position + portal.transform.position;
+
+        camera.fieldOfView = -10 / this.transform.localPosition.z;
+    }
+}
