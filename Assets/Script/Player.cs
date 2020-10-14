4886a21d1dda8e7e0ad480a84a3547a172e99180
felipe <giribroba@gmail.com>
2020-10-13 21:28:10 -0300

    Demo1

---
+++ Assets/Script/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.PlayerLoop;
+
+public class Player : MonoBehaviour
+{
+    public float vel;
+    private Rigidbody rbPlayer;
+
+    void Awake()
+    {
+        rbPlayer = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void FixedUpdate()
+    {
+        print(Input.GetAxis("Horizontal"));
+        rbPlayer.velocity = new Vector3(rbPlayer.velocity.x, rbPlayer.velocity.y, Input.GetAxis("Vertical") * vel);
+    }
+}
